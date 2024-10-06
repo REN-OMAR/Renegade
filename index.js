9ee304d531b8beb6100819f8c36a936bf5e6c6c0
Omar <57677040+REN-OMAR@users.noreply.github.com>
2024-10-06 12:16:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 async function fetchData(){
 
     try{
-
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
   let result="";
     const response2 = await fetch(`https://proclubs.ea.com/api/fc/allTimeLeaderboard?platform=common-gen5`).catch(err=>console.log('err'))
     const movies =  await response2.json().then(c=>{
@@ -14,16 +11,6 @@ async function fetchData(){
       }
   }).catch(err=>result = 'err')
         console.log(movies)
-        if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
-
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById("pokemonSprite");
-
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = "block";
     }
     catch(error){
         console.error(error);
